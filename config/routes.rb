3cf60c6f19d40bd3b98a3dc283cac9fc10ee714f
chitra22 <chitradevir@sowdambikaengg.edu.in>
2016-12-30 10:50:08 +0530

    created admin view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :registers
   devise_for :users
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
